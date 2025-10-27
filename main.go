eed94cc4c1de7470e13183b4ddc6188916efe14b
hababisha <haileamlakderbie1@gmail.com>
2025-10-27 21:56:54 +0300

    add: task1/palindrome.go

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/hababisha/ABT/task1"
 	"github.com/hababisha/ABT/task2"
+	"fmt"
 )
 
 func main(){
 	// fmt.Println("hello world")
 	task1.Hello()
-	task2.FreqCount()
-	task2.Palindrome()
+	// task2.FreqCount()
+	fmt.Println(task2.Palindrome("hello"))
+	fmt.Println(task2.Palindrome("omo"))
 }
--- task2/palindrome.go
+++ task2/palindrome.go
@@ -1,7 +1,27 @@
 package task2
 
-import "fmt"
+import (
+	"unicode"
+)
 
-func Palindrome(){
-	fmt.Println("palindrome")
+func Palindrome(s string) bool{
+	left, right := 0, len(s) -1
+
+
+	
+	for left < right{
+		for !unicode.IsLetter(rune(s[left])){
+			left += 1
+		}
+		for !unicode.IsLetter(rune(s[right])){
+			right -= 1
+		}
+
+		if unicode.ToLower(rune(s[left])) != unicode.ToLower(rune(s[right])){
+			return false
+		}
+		left += 1
+		right -= 1
+	}
+	return true
 }
