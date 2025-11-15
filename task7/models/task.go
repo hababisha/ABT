3798caae9e2f57bb979f082a2e81e80b011ab2bc
hababisha <haileamlakderbie1@gmail.com>
2025-11-15 12:32:57 +0300

    fix: bug fix on task 7 task service

--- task7/models/task.go
+++ task7/models/task.go
@@ -4,7 +4,7 @@ import (
 	"time"
 )
 type Task struct{
-	ID string `json: "id"`
+	ID int `json: "id"`
 	Title string `json: "title`
 	Description string `json: "description"`
 	DueDate time.Time `json:"due_date"`
